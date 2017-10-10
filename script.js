faf49abf14e1e059450212a72baf484573b3aa48
koozy <koozhengyu@gmail.com>
2017-10-10 15:40:20 +0800

    first commit

--- script.js
+++ script.js
@@ -4,8 +4,9 @@ $(function () {
     method: 'POST',
     headers: { 'X-Mashape-Key': 'xONQa6hJi8mshG1BCCGIKmeiCnjNp1UOu2Bjsn7BkDSLsaR4Ua' }
   }
+  var myRequest = new Request(url, init)
   // api call
-  var apiCall = fetch(url, init)
+  var apiCall = fetch(myRequest)
 
   // functions
   var returnResponse = response => {
